e2cdf93398ea53aa2381ea185d65aeb8d0b67642
charpit <charpit@outlook.es>
2022-10-01 15:34:09 -0300

    Agregar archivos de proyecto.

---
+++ FingerprintCombos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FingerprintCombos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
